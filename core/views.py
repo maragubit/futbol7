8d981d97df88992fdb3f18258ad7bf5dd2f75bb0
maragubit <maragubit@gmail.com>
2025-10-10 14:50:35 +0200

    jugadores partidos 0 fuera

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse 
 from jugadores.models import Jugador
+from jugadores.templatetags.jugadores_methods import partidos_jugados
 from partidos.models import Temporada
 
 # Create your views here.
@@ -9,12 +10,13 @@ def home (request):
    return render (request, "index.html")
 
 def clasificacion (request):
-   jugadores=Jugador.objects.all()
    temporadas=Temporada.objects.all()
    if request.GET.get("temporada"):
       temporada=Temporada.objects.get(id=request.GET.get("temporada"))
    else:
        temporada=Temporada.objects.last()
-   jugadores = sorted(Jugador.objects.all(), key=lambda j: (j.puntos(temporada.id), j.golaverage(temporada.id)), reverse=True)
+       
+   jugadores = [j for j in Jugador.objects.all() if j.partidos_jugados(temporada.id) > 0]
+   jugadores = sorted(jugadores, key=lambda j: (j.puntos(temporada.id), j.golaverage(temporada.id)), reverse=True)
    return render (request, "clasificacion.html",{"jugadores":jugadores,"temporada_id":temporada.id,"temporadas":temporadas})
 
